3574685d21851347a976ce43ebb92d245ecf750a
Mateus Prado <mateus@mateusprado.com>
2017-09-10 11:44:07 -0300

    adding test cases

---
+++ test-app.rb
@@ -0,0 +1,20 @@
+ENV['RACK_ENV'] = 'test'
+
+require './app'
+require 'test/unit'
+require 'rack/test'
+
+class HelloWorldTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_home_page
+    get '/'
+    assert last_response.ok?
+    assert_equal 'codeops', last_response.body
+  end
+
+end
